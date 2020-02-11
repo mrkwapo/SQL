6e392b271bedf902b7d22e46dae99b401db419c3
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-02-11 18:51:03 -0500

    Update commands.sql

--- commands.sql
+++ commands.sql
@@ -24,3 +24,11 @@ DROP TABLE Stock;
 
 --delete all data from a table
 DELETE Stock;
+
+
+INSERT INTO Stock (Symbol, Last_Price, Change, Change_Percentage, Currency, Market_Time, Volume, Average_Volume, Market_Cap, ScrapeDate)
+VALUES ('AAPL','2000', '.5', '1%', 'USD', '4:00', '1000', '450', '10000', '2/20/2020');
+
+--Delete and reseed
+DELETE FROM Stock;
+DBCC CHECKIDENT (Stock, RESEED, 0);
